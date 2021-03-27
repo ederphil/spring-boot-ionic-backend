f80b874a25becd55ac392e66910c3f1d7c30b8f7
Ederson Philereno <ederphil@gmail.com>
2021-03-27 15:38:36 -0300

    Projeto criado

---
+++ src/main/java/com/ederphil/cursomc/CursomcApplication.java
@@ -0,0 +1,13 @@
+package com.ederphil.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+}
