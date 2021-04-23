172dec0dbba76b339da1372e359b50f51e18695c
Ederson Philereno <ederphil@gmail.com>
2021-04-23 08:16:51 -0300

    Deletando uma Categoria com Delete

--- src/main/java/com/ederphil/cursomc/controller/CategoriaController.java
+++ src/main/java/com/ederphil/cursomc/controller/CategoriaController.java
@@ -3,6 +3,7 @@ package com.ederphil.cursomc.controller;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -48,6 +49,14 @@ public class CategoriaController {
 		categoria.setId(id);
 		categoria = categoriaService.atualizar(categoria);
 
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+
+		categoriaService.deletar(id);
 		return ResponseEntity.noContent().build();
 
 	}
--- src/main/java/com/ederphil/cursomc/service/CategoriaService.java
+++ src/main/java/com/ederphil/cursomc/service/CategoriaService.java
@@ -3,10 +3,12 @@ package com.ederphil.cursomc.service;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.ederphil.cursomc.domain.Categoria;
 import com.ederphil.cursomc.repository.CategoriaRepository;
+import com.ederphil.cursomc.service.exception.DataIntegrityException;
 import com.ederphil.cursomc.service.exception.ObjectNotFoundException;
 
 @Service
@@ -36,6 +38,18 @@ public class CategoriaService {
 
 		buscar(categoria.getId());
 		return categoriaRepository.save(categoria);
+
+	}
+
+	public void deletar(Integer id) {
+
+		buscar(id);
+		try {
+			categoriaRepository.deleteById(id);
+
+		} catch (DataIntegrityViolationException e) {
+			throw new DataIntegrityException("Não é possível deletar uma Categoria que possui Produtos.");
+		}
 
 	}
 
