c2ce460e662422dc9cb3d1c8b928f90513fe13fe
Ederson Philereno <ederphil@gmail.com>
2021-04-24 06:35:20 -0300

    Paginação com paramentros opcionais na requisição

--- src/main/java/com/ederphil/cursomc/controller/CategoriaController.java
+++ src/main/java/com/ederphil/cursomc/controller/CategoriaController.java
@@ -6,11 +6,14 @@ import java.util.stream.Collector;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -67,8 +70,22 @@ public class CategoriaController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<CategoriaDTO>> findAll() {
 		List<Categoria> list = categoriaService.buscarTodos();
-		List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria)).collect(Collectors.toList());
+		List<CategoriaDTO> listDto = list.stream().map(categoria -> new CategoriaDTO(categoria))
+				.collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDto);
+	}
+
+	@RequestMapping(value = "/page", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPage", defaultValue = "24") Integer linesPage,
+			@RequestParam(value = "orderBy", defaultValue = "nome") String orderBy,
+			@RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+		Page<Categoria> list = categoriaService.findPage(page, linesPage, orderBy, direction);
+		Page<CategoriaDTO> listDto = list.map(categoria -> new CategoriaDTO(categoria));
+		
+		return ResponseEntity.ok().body(listDto);
+
 	}
 
 }
